c6bf27341240e68ad9ddadddd79c3c28bae10a96
Solomon Foster <colomon@gmail.com>
2012-12-20 16:53:04 -0500

    Two new scripts, fixed thinko in existing script.

--- bin/collatz-recursive-ternary-hand-cached.pl
+++ bin/collatz-recursive-ternary-hand-cached.pl
@@ -12,9 +12,11 @@ multi sub trait_mod:<is>(Routine $r, :$cached!) {
     );
 }
 
-sub collatz-length(Int $n) is cached {
+sub collatz-length(Int $n) {
     return 1 if $n == 1;
-    1 + ($n %% 2 ?? collatz-length($n div 2) !! collatz-length(3 * $n + 1));
+    state %lengths;
+    return %lengths{$n} if %lengths.exists($n);
+    %lengths{$n} = 1 + ($n %% 2 ?? collatz-length($n div 2) !! collatz-length(3 * $n + 1));
 }
 
 sub MAIN(*@numbers) {
---
+++ bin/timing-table-generator.pl
@@ -0,0 +1,31 @@
+my @numbers = 1..200, 10000..10200;
+my $rakudo = "perl6";
+my $niecza = "mono ../niecza/run/Niecza.exe";
+
+sub MAIN(*@scripts) {
+    my %rakudo-results;
+    my %niecza-results;
+    for @scripts -> $script {
+        my $start = now;
+        qqx/$rakudo $script { @numbers }/;
+        my $end = now;
+        %rakudo-results{$script} = ($end - $start).round(1/100);
+
+        next if $script ~~ /cached/ && $script !~~ /"hand-cached"/;
+        
+        $start = now;
+        qqx/$niecza $script { @numbers }/;
+        $end = now;
+        %niecza-results{$script} = ($end - $start).round(1/100);
+    }
+
+    say '<table border="1" align="center"> <tr><td>Script</td><td>Rakudo</td><td>Niecza</td></tr>';
+    for %rakudo-results.pairs.sort(*.value) -> (:key($script), :value($time)) {
+        say "<tr>";
+        say "    <td> <a href=\"https://github.com/colomon/perl6-collatz/blob/master/$script\"> $script </a> </td>";
+        say "    <td> $time </td>";
+        say "    <td> { %niecza-results{$script} // "N/A" } </td>";
+        say "</tr>";
+    }
+    say "</table>";
+}
